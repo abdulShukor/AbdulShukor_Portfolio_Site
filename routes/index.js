96d1d58ebe0dc616602f0a0492fd502f419ecb85
abdulShukor <ashukoor12@gmail.com>
2019-01-28 16:07:39 -0500

    Created Five pages ExpressJs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+
 
 module.exports = router;
