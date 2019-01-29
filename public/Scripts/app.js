f4bfd5ab68cfc4229713d5e1ed8e18c4f9fb4361
abdulShukor <ashukoor12@gmail.com>
2019-01-29 11:30:35 -0500

    Added Bootstrap

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,14 @@
-/* Custom javaSript goes here */
+
+/* Custome javaSript goes here */
+
+(function(){
+
+
+    function Start(){
+        console.log(`%c App started..`, "font-size:20px;color:blue;font-weight:bold");
+
+    }
+
+window.addEventListener("load", Start);
+
+}());
